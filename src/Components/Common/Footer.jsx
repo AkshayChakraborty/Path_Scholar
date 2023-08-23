11cff209b194b5ee0699f7bfa95dcfd569146c58
AkshayChakraborty <akshaychakraborty877@gmail.com>
2023-08-23 13:19:40 +0530

    project

--- src/Components/Common/Footer.jsx
+++ src/Components/Common/Footer.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 const Footer = () => {
   return (
@@ -10,7 +11,7 @@ const Footer = () => {
     <div className="container">
       <div className="row">
         <div className="col-lg-3 col-md-6 footer-contact">
-          <h3>Company</h3>
+          <h3>Path Scholar</h3>
           <p>
             A108 Adam Street <br />
             New York, NY 535022<br />
@@ -22,9 +23,9 @@ const Footer = () => {
         <div className="col-lg-2 col-md-6 footer-links">
           <h4>Useful Links</h4>
           <ul>
-            <li><i className="bx bx-chevron-right" /> <a href="#">Home</a></li>
-            <li><i className="bx bx-chevron-right" /> <a href="#">About us</a></li>
-            <li><i className="bx bx-chevron-right" /> <a href="#">Services</a></li>
+            <li><i className="bx bx-chevron-right" /> <NavLink to="/">Home</NavLink></li>
+            <li><i className="bx bx-chevron-right" /> <NavLink to="/about">About us</NavLink></li>
+            <li><i className="bx bx-chevron-right" /> <NavLink to="/courses">Courses</NavLink></li>
             <li><i className="bx bx-chevron-right" /> <a href="#">Terms of service</a></li>
             <li><i className="bx bx-chevron-right" /> <a href="#">Privacy policy</a></li>
           </ul>
@@ -52,14 +53,14 @@ const Footer = () => {
   <div className="container d-md-flex py-4">
     <div className="mr-md-auto text-center text-md-left">
       <div className="copyright">
-        © Copyright <strong><span>Company</span></strong>. All Rights Reserved
+        © Copyright <strong><span>Path Scholar</span></strong>. All Rights Reserved
       </div>
       <div className="credits">
         {/* All the links in the footer should remain intact. */}
         {/* You can delete the links only if you purchased the pro version. */}
         {/* Licensing information: https://bootstrapmade.com/license/ */}
         {/* Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/company-free-html-bootstrap-template/ */}
-        Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+        {/* Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a> */}
       </div>
     </div>
     <div className="social-links text-center text-md-right pt-3 pt-md-0">
